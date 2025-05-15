3764aa89b02fdcb2d4ad8a89ed3a0369b9fd4b88
rubenromani <ruben.romani@outlook.it>
2025-05-15 19:28:46 +0200

    refactored cpu.c and cpu.h

--- c_impl/src/cpu.c
+++ c_impl/src/cpu.c
@@ -1,19 +1,67 @@
+#include <stdio.h>
+
 #include "cpu.h"
-#include "bus.h"
+#include "instructions.h"
 
-static void update_pc(struct cpu *cpu, uint8_t val);
-
-struct cpu cpu = {
-        .regs.bytes = {0x00},
-        .ops = {
-                .execute = execution,
-                .update_pc = update_pc
-        },
-        .bus = &bus
+// external variables
+struct cpu_context context = {
+        .regs.bytes = {0} 
 };
 
-static void update_pc(struct cpu *cpu, uint8_t val)
+// Local function protorypes
+static void update_pc(struct cpu_context * context, uint8_t val);
+static uint8_t read_byte(struct cpu_context * context, uint16_t addr);
+static uint16_t read_word(struct cpu_context * context, uint16_t addr);
+static void write_byte(struct cpu_context * context, uint16_t addr, uint8_t val);
+static void write_word(struct cpu_context * context, uint16_t addr, uint16_t val);
+static struct instruction fetch(struct cpu_context * context);
+
+// Public functions
+
+void run(struct cpu_context * context)
 {
-        cpu->regs.word.pc += val;
+        printf("Start running execution\n");
 }
 
+// Local functions
+
+static void update_pc(struct cpu_context * context, uint8_t val)
+{
+        context->regs.words[PC] += val;
+}
+
+static uint8_t read_byte(struct cpu_context * context, uint16_t addr)
+{
+        update_pc(context, 1);
+        
+         // read from memory -> to externalize
+        return mem[addr];
+}
+
+static uint16_t read_word(struct cpu_context * context, uint16_t addr)
+{
+        printf("NOT IMPLEMENTED");
+}
+
+static void write_byte(struct cpu_context * context, uint16_t addr, uint8_t val)
+{
+        printf("NOT IMPLEMENTED");
+}
+
+static void write_word(struct cpu_context * context, uint16_t addr, uint8_t val)
+{
+        printf("NOT IMPLEMENTED");
+}
+
+static instruction fetch(struct cpu_context * context)
+{
+        uint8_t opcode = read_byte(context, context->regs.words[PC]);
+        return get_instruction(opcode);
+}
+
+int main()
+{
+        printf("Hello from CPU\n");
+        run(&context);
+        return 0;
+}
--- c_impl/src/include/cpu.h
+++ c_impl/src/include/cpu.h
@@ -3,9 +3,6 @@
 
 #include <stdint.h>
 
-#include "instructions.h"
-#include "bus.h"
-
 enum cpu_reg_byte
 {
         A = 0,
@@ -33,12 +30,12 @@ union cpu_registers
         {
                 uint8_t a;
                 uint8_t f;
-                uint8_t b;
                 uint8_t c;
-                uint8_t d;
+                uint8_t b;
                 uint8_t e;
-                uint8_t h;
+                uint8_t d;
                 uint8_t l;
+                uint8_t h;
                 uint8_t reserved[4];
         } byte;
 
@@ -70,20 +67,16 @@ union cpu_registers
         
 };
 
-typedef void(*update_pc_fn)(struct cpu *cpu, uint8_t val);
-
-struct cpu_ops{
-        const ins_execution *execute;
-        const update_pc_fn update_pc;
-};
-
-struct cpu
+struct cpu_context
 {
         union cpu_registers regs;
-        struct cpu_ops ops;
-        struct system_bus *bus;
 };
 
-extern struct cpu cpu;
+void run(struct cpu_context * context);
+
+extern struct cpu_context context;
+
+// Temporary
+extern uint8_t mem[0xFFFF];
 
 #endif /* _CPU_H */
